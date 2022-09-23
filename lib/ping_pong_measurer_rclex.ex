fb0341579707ff9aad715d6880541d5e638f2461
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-09-23 11:38:56 +0900

    Initial commit

---
+++ lib/ping_pong_measurer_rclex.ex
@@ -0,0 +1,18 @@
+defmodule PingPongMeasurerRclex do
+  @moduledoc """
+  Documentation for `PingPongMeasurerRclex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PingPongMeasurerRclex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ping_pong_measurer_rclex_test.exs
@@ -0,0 +1,8 @@
+defmodule PingPongMeasurerRclexTest do
+  use ExUnit.Case
+  doctest PingPongMeasurerRclex
+
+  test "greets the world" do
+    assert PingPongMeasurerRclex.hello() == :world
+  end
+end
