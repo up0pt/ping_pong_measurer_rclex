21cd510e23b873f06280719a3abe7da2eabc926d
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-09-27 11:24:26 +0900

    Refactor MeasurementHelper

--- lib/measurement_helper.ex
+++ lib/measurement_helper.ex
@@ -3,14 +3,14 @@ defmodule MeasurementHelper do
 
   alias PingPongMeasurerRclex.Data
 
-  def start_measurement(node_count \\ 1, payload_bytes \\ 10, measurement_times \\ 10)
-      when node_count in [1, 10, 100] and payload_bytes in [10, 100, 1000, 10000] do
-    data_directory_path = prepare_data_directory!(node_count, payload_bytes, measurement_times)
+  def start_measurement(node_counts \\ 1, payload_bytes \\ 10, measurement_times \\ 10)
+      when node_counts in [1, 10, 100] and payload_bytes in [10, 100, 1000, 10000] do
+    data_directory_path = prepare_data_directory!(node_counts, payload_bytes, measurement_times)
 
     context = Rclex.rclexinit()
 
-    # PingPongMeasurerRclex.start_pong_processes(context, node_count)
-    PingPongMeasurerRclex.start_ping_processes(context, node_count, data_directory_path)
+    # PingPongMeasurerRclex.start_pong_processes(context, node_counts)
+    PingPongMeasurerRclex.start_ping_processes(context, node_counts, data_directory_path)
 
     for i <- 1..measurement_times do
       PingPongMeasurerRclex.start_ping_pong(String.duplicate("a", payload_bytes))
@@ -24,7 +24,7 @@ defmodule MeasurementHelper do
     PingPongMeasurerRclex.stop_ping_processes()
   end
 
-  defp prepare_data_directory!(process_count, payload_bytes, measurement_times) do
+  defp prepare_data_directory!(node_counts, payload_bytes, measurement_times) do
     data_directory_path =
       Application.get_env(:ping_pong_measurer_rclex, :data_directory_path) ||
         raise """
@@ -33,7 +33,7 @@ defmodule MeasurementHelper do
         """
 
     dt_string = Data.datetime_to_string(DateTime.utc_now())
-    directory_name = "#{dt_string}_pc#{process_count}_pb#{payload_bytes}_mt#{measurement_times}"
+    directory_name = "#{dt_string}_pc#{node_counts}_pb#{payload_bytes}_mt#{measurement_times}"
     data_directory_path = Path.join(data_directory_path, directory_name)
 
     File.mkdir_p!(data_directory_path)
